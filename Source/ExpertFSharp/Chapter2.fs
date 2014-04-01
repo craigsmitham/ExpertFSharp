99603004c68498eae4d77165c4119b88f4b68789
Craig Smitham <craigsmitham@gmail.com>
2014-03-31 19:45:02 -0500

    chapter 2 examples

---
+++ Source/ExpertFSharp/Chapter2.fs
@@ -0,0 +1,21 @@
+﻿module Chapter2
+
+let splitAtSpaces (text: string) =
+    text.Split ' '
+    |> Array.toList
+
+let wordCount text =
+    let words = splitAtSpaces text
+    let wordSet = Set.ofList words
+    let numWords = words.Length
+    let numDups = words.Length - wordSet.Count
+    (numWords, numDups)
+
+let showWordCount text =
+    let numWords, numDups = wordCount text
+    printfn "--> %d words in the text" numWords
+    printfn "--> %d duplicate words" numDups
+
+let powerOfFour n =
+    let nSquared = n * n 
+    nSquared * nSquared
